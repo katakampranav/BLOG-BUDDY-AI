5cfa998404ea4a76bad6045f5bd0054888aceb52
unknown <katakampranavshankar@gmail.com>
2025-01-15 01:27:49 +0530

    changes

--- server/app.py
+++ server/app.py
@@ -54,5 +54,4 @@ def generateBlog():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(host='0.0.0.0', port=5000)
